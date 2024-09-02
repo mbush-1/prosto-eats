ca41da5d6a83f7e050c7d11711f2f035aeebc0b4
mbush-1 <prekronder@gmail.com>
2024-09-01 20:25:10 -0400

    added buttons to main menu

--- prosto-eats/files/recipe-loader.js
+++ prosto-eats/files/recipe-loader.js
@@ -5,14 +5,14 @@ console.log('I started!')
 
 if (window.location.pathname.includes('all-recipes.html')) {
     // If on the recipes page, render all recipes
-    renderLocation = '.all-recipes'; // Ensure this matches the class in your recipes.html
+    renderLocation = '.all-recipes' // Ensure this matches the class in your recipes.html
     relativeDataLocation = 'files/data.json'
     relativeImagePath = 'images/recipe-thumbnails/'
     
     console.log('in all recipes right now!')
 } else {
     // If on the home page, render featured recipes
-    renderLocation = '.featured-recipes';
+    renderLocation = '.featured-recipes'
     relativeDataLocation = 'prosto-eats/files/data.json'
     relativeImagePath = 'prosto-eats/images/recipe-thumbnails/'
     // console.log('in featured recipes right now!')
@@ -39,53 +39,59 @@ fetch(relativeDataLocation)
 function renderAllRecipes(recipes, location) {
     recipes.forEach(recipe => {
         renderRecipe(recipe, location)
-    });
+    })
 
 }
 
 function renderRecipe(recipe, location) {
     // Determine where to render the recipe
-    const devLocation = document.querySelector(location);
+    const devLocation = document.querySelector(location)
 
     // Render the structure
-    const containerBox = document.createElement('div');
-    containerBox.className = 'col-md-4 mb-3';
-    // containerBox.style.cursor = 'pointer';
+    const containerBox = document.createElement('div')
+    containerBox.className = 'col-md-4 mb-3'
+    // containerBox.style.cursor = 'pointer'
 
-    const cardBox = document.createElement('div');
-    cardBox.className = 'card';
-    containerBox.appendChild(cardBox);
+    const cardBox = document.createElement('div')
+    cardBox.className = 'card'
+    containerBox.appendChild(cardBox)
 
     // image
-    const recipeThumbnail = document.createElement('img');
-    recipeThumbnail.setAttribute('src', (relativeImagePath + recipe['image-source']));
-    recipeThumbnail.className = 'image-fluid w-100 recipe-thumbnail';
-    cardBox.appendChild(recipeThumbnail);
+    const recipeThumbnail = document.createElement('img')
+    recipeThumbnail.setAttribute('src', (relativeImagePath + recipe['image-source']))
+    recipeThumbnail.className = 'image-fluid w-100 recipe-thumbnail'
+    cardBox.appendChild(recipeThumbnail)
 
-    const cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
+    const cardBody = document.createElement('div')
+    cardBody.className = 'card-body'
 
-    const recipeTitle = document.createElement('h5');
-    recipeTitle.className = 'card-title';
-    recipeTitle.setAttribute('style', 'font-family: Kanit; font-weight: 500;')
+    const recipeTitle = document.createElement('h5')
+    recipeTitle.className = 'card-title'
+    recipeTitle.setAttribute('style', 'font-family: Kanit font-weight: 500')
     recipeTitle.textContent = recipe['name'] + ' • ' + recipe['time-to-cook'] + ' Minutes'
-    cardBody.appendChild(recipeTitle);
+    cardBody.appendChild(recipeTitle)
 
-    const recipeSubtitle = document.createElement('h6');
-    recipeSubtitle.className = 'card-subtitle text-muted mb-2';
-    recipeSubtitle.setAttribute('style', 'font-family: Kanit; font-weight: 500;')
+    const recipeSubtitle = document.createElement('h6')
+    recipeSubtitle.className = 'card-subtitle text-muted mb-2'
+    recipeSubtitle.setAttribute('style', 'font-family: Kanit font-weight: 500')
     recipeSubtitle.textContent = recipe['origin'] + ' Origin'
-    cardBody.appendChild(recipeSubtitle);
+    cardBody.appendChild(recipeSubtitle)
 
-    const recipeDescription = document.createElement('p');
-    recipeDescription.className = 'page-body-text';
-    recipeDescription.textContent = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, assumenda nemo ipsum labore maxime vel.';
-    cardBody.appendChild(recipeDescription);
+    const recipeDescription = document.createElement('p')
+    recipeDescription.className = 'page-body-text'
+    recipeDescription.textContent = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, assumenda nemo ipsum labore maxime vel.'
+    cardBody.appendChild(recipeDescription)
 
-    cardBox.appendChild(cardBody);
+    // const seeRecipeButton = document.createElement('button')
+    // seeRecipeButton.className = 'btn btn-primary'
+    // seeRecipeButton.textContent = 'See Recipe'
+    // cardBody.appendChild(seeRecipeButton)
+
+
+    cardBox.appendChild(cardBody)
 
     // Append the container box to the specified location
-    devLocation.appendChild(containerBox);
+    devLocation.appendChild(containerBox)
 }
 
 function renderFeaturedRecipes(recipes, location) {
@@ -95,7 +101,7 @@ function renderFeaturedRecipes(recipes, location) {
 
     const featuredIndexes = []
     while (featuredIndexes.length < 3) {
-        const randomNumber = Math.floor(Math.random() * numRecipes);
+        const randomNumber = Math.floor(Math.random() * numRecipes)
         if (!featuredIndexes.includes(randomNumber)) {
             featuredIndexes.push(randomNumber)
         }
